66c926a0f03edcbb656e99c030b016d949a91252
Handstrings <pulling@handstrings.com>
2014-05-04 16:18:18 -0400

    Improve Prompt a little.

--- Resources/app.nw/public/js/prompt.js
+++ Resources/app.nw/public/js/prompt.js
@@ -1,88 +1,88 @@
+/*jslint node: true, browser: true, devel:true, white: false*/
 (function (global) {
-  var PROMPT  = {}
-    , p       = document.getElementById("prompt")
-    , pTitle  = document.getElementById("prompt-title")
-    , pText   = document.getElementById("prompt-text")
-    , pButtons= document.getElementById("prompt-buttons");
- 
+    var PROMPT  = {},
+        p = document.getElementById("prompt"),
+        pTitle = document.getElementById("prompt-title"),
+        pText = document.getElementById("prompt-text"),
+        pButtons = document.getElementById("prompt-buttons");
 
-  PROMPT.show = function () {
-    p.style.display = "block";
-  };
+    PROMPT.show = function () {
+        p.style.display = "block";
+    };
 
-  PROMPT.hide = function () {
-    p.style.display = "none";
-  };
+    PROMPT.hide = function () {
+        p.style.display = "none";
+    };
 
-  PROMPT.clear = function () {
-    pButtons.innerHTML = "";
-    pTitle.innerText = "";
-    pText.innerText = "";
-  };
+    PROMPT.clear = function () {
+        pButtons.innerHTML = "";
+        pTitle.innerText = "";
+        pText.innerText = "";
+    };
 
-  PROMPT.init = function(title, text) {
-    if (text === undefined) {
-      text = title;
-      title = "";
-    }
+    PROMPT.init = function (title, text) {
+        if (text === undefined) {
+            text = title;
+            title = "";
+        }
 
-    pTitle.innerText = title;
-    pText.innerText = text;
-    pButtons.innerHTML = "";
-  };
+        pTitle.innerText = title;
+        pText.innerText = text;
+        pButtons.innerHTML = "";
+    };
 
-  PROMPT.init.prototype.addBtn = function (opts) {
-    if (typeof opts === "object") {
-      var text = opts.text ? opts.text : ""
-        , id   = opts.id ? opts.id : ""
-        , call = opts.onclick
-        , type = opts.type ? opts.type : ""
-        , focus = opts.focus ? opts.focus : false
-        , disabled = opts.disabled ? true : false
-        , autofocus
-        , btn;
+    PROMPT.init.prototype.addBtn = function (opts) {
+        if (typeof opts === "object") {
+            var text = opts.text || "",
+                id   = opts.id || "",
+                call = opts.onclick,
+                type = opts.type || "",
+                focus = opts.focus || false,
+                disabled = opts.disabled ? true : false,
+                autofocus,
+                btn;
 
-      if (focus === true) {
-        autofocus = "autofocus='autofocus'";
-      } else {
-        autofocus = "";
-      }
+            if (focus === true) {
+                autofocus = "autofocus='autofocus'";
+            } else {
+                autofocus = "";
+            }
 
-      btn           = document.createElement("button");
-      btn.className = "btn " + type;
-      btn.id        = id;
-      if (focus) {
-        btn.autofocus = autofocus;
-      }
-      if (disabled) {
-        btn.disabled = "disabled";
-      }
-      btn.innerText = text;
+            btn           = document.createElement("button");
+            btn.className = "btn " + type;
+            btn.id        = id;
+            if (focus) {
+                btn.autofocus = autofocus;
+            }
+            if (disabled) {
+                btn.disabled = "disabled";
+            }
+            btn.innerText = text;
 
-      pButtons.appendChild(btn);
+            pButtons.appendChild(btn);
 
-      if (call !== undefined) {
-        btn.addEventListener("click", function (e) {
-          call(e);
-          PROMPT.hide();
-          PROMPT.clear();
-        });
-      }
-    }
-    return this;
-  };
+            if (call !== undefined) {
+                btn.addEventListener("click", function (e) {
+                    call(e);
+                    PROMPT.hide();
+                    PROMPT.clear();
+                });
+            }
+        }
+        return this;
+    };
 
-  PROMPT.init.prototype.show = function () {
-    PROMPT.show();
-  };
+    PROMPT.init.prototype.show = function () {
+        PROMPT.show();
+    };
 
-  PROMPT.init.prototype.hide = function () {
-    PROMPT.hide();
-  };
+    PROMPT.init.prototype.hide = function () {
+        PROMPT.hide();
+    };
 
-  PROMPT.init.prototype.clear = function () {
-    PROMPT.clear();
-  };
- 
-  global.PROMPT = PROMPT;
+    PROMPT.init.prototype.clear = function () {
+        PROMPT.clear();
+    };
+
+    global.PROMPT = PROMPT;
 }(this));
